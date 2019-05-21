f4d65f6ae881bd065ea3e3a3a9449b472bd530e3
fabio santos <fs.sep@hotmail.com>
2019-05-21 12:46:22 -0300

    finalização da animação

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,12 +4,13 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: #8b10ae;
-  align-items: center;
   justify-content: center;
 `;
 
 export const Content = styled.View`
   flex: 1;
+  align-items: center;
+  justify-content: center;
   max-height: 400px;
   z-index: 5;
   margin-bottom: 10px;
@@ -24,7 +25,7 @@ export const Card = styled(Animated.View)`
   position: absolute;
   left: 0;
   right: 0;
-  top: 0px;
+  top: 0;
 `;
 
 export const CardHeader = styled.View`
